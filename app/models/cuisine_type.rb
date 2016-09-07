b6ecdad71c3a0b7352fb4b0f4fe2ab4850ba2087
Oded Magger <oded.magger@wework.com>
2016-09-07 14:59:38 +0300

    Validation on models data fields.

--- app/models/cuisine_type.rb
+++ app/models/cuisine_type.rb
@@ -1,4 +1,7 @@
 class CuisineType < ApplicationRecord
 
   has_many :restaurants
+
+  validates :name, :image_icon, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,9 @@
 class Restaurant < ApplicationRecord
 
   belongs_to :cuisine_type, optional: true
+
+  validates :name, :address, presence: true
+  validates :name, uniqueness: true
+  validates :delivery_time, numericality: { greater_than_or_equal_to: 0 }
+  validates :rating, :inclusion => { :in => 0..3 }
 end
